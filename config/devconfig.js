6b32ca790f33e9b8c6d645188f73447f06512a26
lizeding <15118279909li@sina.com>
2018-02-24 18:34:09 +0800

    Update devconfig.js

--- config/devconfig.js
+++ config/devconfig.js
@@ -1,8 +1,6 @@
 export default {
   dev: {
     server: 'http://test2.vellgo.com.cn/app/',
-    //server: "http://trgf.vellgo.com.cn/app/",
-    //server: "http://192.168.3.153:8081/app/",
     jumpAddress: '/app/trgf_app/scanBack.html',
     createQRcode: 'http://pan.baidu.com/share/qrcode?w=200&h=200&url='
   }
